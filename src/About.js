ae9b9c15bacde690d62f7a3c36c597329b14a358
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-11-28 17:18:04 -0600

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,21 +5,21 @@ import lindyPic from "./assets/lindy-bio.png"
 class About extends Component {
     render() {
         return (
-            <div className="test">
-                <div>
-                    <img src={lindyPic} alt="Image of author" className="author-image" />
+            <>
+               
+                <img src={lindyPic} alt="Image of author" className="author-image" />
+                
+                <div className="author-bio">
+                    <p>M. J. Lindemann is the author of an upcoming epic fantasy trilogy. He lives with his husband and two dogs in Minnesota. In addition to writing, he works as a public health nurse, helping to connect people to home services.</p>
+
+                    <p>  When he's not spending every moment of his free time writing, he plays video games, watches an unholy amount of T.V., plays D&D, and reads (of course). His favorite genres are science fiction and fantasy, but he'll dive into anything that's a good story.</p>
+
+                    <p>   What you'll find in his writing: Found family. Rich worlds. Diverse casts of characters. Heartwarming moments. Tragedy and hope.</p>
+
+                    <p>  What you won't find: Sexual assault of any kind. Racism. Sexism. Homophobia or transphobia.</p>
+
                 </div>
-                <div>
-                    <p className="author-bio">M. J. Lindemann is the author of an upcoming epic fantasy trilogy. He lives with his husband and two dogs in Minnesota. In addition to writing, he works as a public health nurse, helping to connect people to home services.
-
-                        When he’s not spending every moment of his free time writing, he plays video games, watches an unholy amount of T.V., plays D&D, and reads (of course). His favorite genres are science fiction and fantasy, but he’ll dive into anything that’s a good story.
-
-                        What you’ll find in his writing: Found family. Rich worlds. Diverse casts of characters. Heartwarming moments. Tragedy and hope.
-
-                        What you won’t find: Sexual assault of any kind. Racism. Sexism. Homophobia or transphobia.</p>
-
-                </div>
-            </div>
+           </>
         );
     }
 }
