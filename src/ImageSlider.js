0b30033ef8f5ad7d2690b8c36c131409393def8e
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-12-11 13:12:29 -0600

    fixed transistions for the most part

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,51 +1,45 @@
 import { useState, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
-
 
 
 function ImageSlider({ imageUrls, imageText }) {
 
     const [imageIndex, setImageIndex] = useState(0);
-    const [inProp, setInProp] = useState(true);
 
     return (
-        <div className='slider-container'>
-            <CSSTransition
-                in={inProp} // Set to true to make the initial transition happen
-                timeout={500} // Adjust as needed
-                classNames='fade'
-                unmountOnExit
-                onEnter={() => setInProp(true)}
-                onExited={() => setInProp(false)}
-            >
-                <div className='img-container'>
-                    <img src={imageUrls[imageIndex]}
-                        alt={`Image ${imageIndex}`}
-                        className='img-slider-img'
-                        style={{
-                            opacity: inProp ? 1 : 0,
-                            transform: `translateX(${inProp ? 0 : '100%'})`,
-                            transition: 'opacity 1s, transform 1s',
-                        }}
-                    />
+        <>
+            <div className='slider-container'>
+
+                <div className='img-container' style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
+                    {imageUrls.map(url => (
+
+                        <img key={url} src={url} className='img-slider-img' style={{ translate: `${-100 * imageIndex}%` }} />
+
+                    ))}
                 </div>
-            </CSSTransition>
-            <div className='description-container'>
-                <p className='book-description'>{imageText[imageIndex]}</p>
+
+
+                <div className='description-container'>
+                    <p className='book-description'>{imageText[imageIndex]}</p>
+                </div>
             </div>
             <button
                 className='img-slider-left-btn'
                 onClick={() => setImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length)}
             >
-                &#8656;
             </button>
             <button
                 className='img-slider-right-btn'
                 onClick={() => setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)}
             >
-                &#8658;
             </button>
-        </div>
+            <div className='dot-container'>
+                {imageUrls.map((_, index) => (
+                    <button key={index} className='img-slider-dot-btn' onClick={() => setImageIndex(index)}>
+                        {index === imageIndex ? String.fromCharCode(9673) : String.fromCharCode(9675)}
+                    </button>
+                ))}
+            </div>
+        </>
     );
 }
 
