5b0a7fbaa15c5524adf508b8b2364b4ea36b9fca
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-12-11 15:47:11 -0600

    fixed transitions

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -7,38 +7,62 @@ function ImageSlider({ imageUrls, imageText }) {
 
     return (
         <>
-            <div className='slider-container'>
+            <div className='main-slider-container'>
+                <button
+                    className='img-slider-left-btn'
+                    onClick={() => setImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length)}
+                >
+                </button>
+                <div className='book-slider-container'>
+                    <div className='img-container' style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
+                        {imageUrls.map((url, index) => (
 
-                <div className='img-container' style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
-                    {imageUrls.map(url => (
+                            <img
+                                key={url}
+                                src={url}
+                                className={`img-slider-img ${index === imageIndex ? 'visible' : ''}`}
+                                style={{
+                                    opacity: index === imageIndex ? 1 : 0,
+                                }}
+                            />
+                        ))}
+                    </div>
+                    <div className='dot-container'>
+                        {imageUrls.map((_, index) => (
+                            <button key={index} className='img-slider-dot-btn' onClick={() => setImageIndex(index)}>
+                                {index === imageIndex ? String.fromCharCode(9673) : String.fromCharCode(9675)}
+                            </button>
+                        ))}
+                    </div>
+                </div>
 
-                        <img key={url} src={url} className='img-slider-img' style={{ translate: `${-100 * imageIndex}%` }} />
 
+                <div className='description-container' style={{ display: "flex", overflow: "hidden" }}>
+
+
+                    {imageText.map((imageText, index) => (
+                        <p
+                            key={index}
+                            className={`book-description ${index === imageIndex ? 'visible' : ''}`}
+                            style={{
+                                opacity: index === imageIndex ? 1 : 0,
+                            }}
+                        >
+                            {imageText}
+                        </p>
                     ))}
-                </div>
 
 
-                <div className='description-container'>
-                    <p className='book-description'>{imageText[imageIndex]}</p>
                 </div>
+
+                <button
+                    className='img-slider-right-btn'
+                    onClick={() => setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)}
+                >
+                </button>
             </div>
-            <button
-                className='img-slider-left-btn'
-                onClick={() => setImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length)}
-            >
-            </button>
-            <button
-                className='img-slider-right-btn'
-                onClick={() => setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)}
-            >
-            </button>
-            <div className='dot-container'>
-                {imageUrls.map((_, index) => (
-                    <button key={index} className='img-slider-dot-btn' onClick={() => setImageIndex(index)}>
-                        {index === imageIndex ? String.fromCharCode(9673) : String.fromCharCode(9675)}
-                    </button>
-                ))}
-            </div>
+
+
         </>
     );
 }
