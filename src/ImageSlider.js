90dec9c2f184d1c15e32b323f5a764c4edf21c92
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-12-11 19:03:44 -0600

    added mobile view for books page

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -13,46 +13,48 @@ function ImageSlider({ imageUrls, imageText }) {
                     onClick={() => setImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length)}
                 >
                 </button>
-                <div className='book-slider-container'>
-                    <div className='img-container' style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
-                        {imageUrls.map((url, index) => (
+                <div className='mobile-slider-container'>
+                    <div className='book-slider-container'>
+                        <div className='img-container' style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
+                            {imageUrls.map((url, index) => (
 
-                            <img
-                                key={url}
-                                src={url}
-                                className={`img-slider-img ${index === imageIndex ? 'visible' : ''}`}
+                                <img
+                                    key={url}
+                                    src={url}
+                                    className={`img-slider-img ${index === imageIndex ? 'visible' : ''}`}
+                                    style={{
+                                        opacity: index === imageIndex ? 1 : 0,
+                                    }}
+                                />
+                            ))}
+                        </div>
+                        <div className='dot-container'>
+                            {imageUrls.map((_, index) => (
+                                <button key={index} className='img-slider-dot-btn' onClick={() => setImageIndex(index)}>
+                                    {index === imageIndex ? String.fromCharCode(9673) : String.fromCharCode(9675)}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+
+
+                    <div className='description-container' style={{ display: "flex", overflow: "hidden" }}>
+
+
+                        {imageText.map((imageText, index) => (
+                            <p
+                                key={index}
+                                className={`book-description ${index === imageIndex ? 'visible' : ''}`}
                                 style={{
                                     opacity: index === imageIndex ? 1 : 0,
                                 }}
-                            />
+                            >
+                                {imageText}
+                            </p>
                         ))}
+
+
                     </div>
-                    <div className='dot-container'>
-                        {imageUrls.map((_, index) => (
-                            <button key={index} className='img-slider-dot-btn' onClick={() => setImageIndex(index)}>
-                                {index === imageIndex ? String.fromCharCode(9673) : String.fromCharCode(9675)}
-                            </button>
-                        ))}
-                    </div>
-                </div>
-
-
-                <div className='description-container' style={{ display: "flex", overflow: "hidden" }}>
-
-
-                    {imageText.map((imageText, index) => (
-                        <p
-                            key={index}
-                            className={`book-description ${index === imageIndex ? 'visible' : ''}`}
-                            style={{
-                                opacity: index === imageIndex ? 1 : 0,
-                            }}
-                        >
-                            {imageText}
-                        </p>
-                    ))}
-
-
                 </div>
 
                 <button
--- src/Main.js
+++ src/Main.js
@@ -19,12 +19,22 @@ const IMAGE_DESCRIPTIONS = [
     "Nibh mauris cursus mattis molestie. Viverra nam libero justo laoreet sit amet cursus. Feugiat pretium nibh ipsum consequat nisl. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Tellus at urna condimentum mattis pellentesque id. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Ullamcorper sit amet risus nullam eget felis eget. Enim nec dui nunc mattis enim ut. Id neque aliquam vestibulum morbi blandit cursus risus. Egestas tellus rutrum tellus pellentesque eu. Massa ultricies mi quis hendrerit dolor magna eget. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. A scelerisque purus semper eget duis. Vestibulum mattis ullamcorper velit sed ullamcorper.",
     "Eget gravida cum sociis natoque penatibus et magnis. Id aliquet risus feugiat in ante metus dictum at. Nisi est sit amet facilisis magna etiam tempor. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Tortor at risus viverra adipiscing at in. Non diam phasellus vestibulum lorem sed risus. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Vestibulum morbi blandit cursus risus at ultrices mi tempus. In ante metus dictum at. Neque gravida in fermentum et sollicitudin ac orci phasellus. Morbi leo urna molestie at. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Blandit massa enim nec dui nunc. Nunc mattis enim ut tellus. Et malesuada fames ac turpis egestas maecenas. In hac habitasse platea dictumst quisque sagittis purus sit amet. Et ligula ullamcorper malesuada proin. Bibendum est ultricies integer quis auctor elit sed vulputate. Euismod elementum nisi quis eleifend."]
 
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
+
 function Main() {
     return (
         <Router>
             <>
                 <header>
                     <h1><NavLink to="/">M. J. Lindemann</NavLink></h1>
+                    <div className="menu-icon" onClick={toggleMenu}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
                     <ul className="navbar">
                         <li><NavLink to="/books">Books</NavLink></li>
                         <li><NavLink to="/about">About</NavLink></li>
