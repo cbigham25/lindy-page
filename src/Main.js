d87f46c59a5442e9b4f656342e63f4a2c6eeb6b5
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2024-01-05 08:54:42 -0600

    fixed book navigation

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     Route,
     NavLink,
     BrowserRouter as Router,
     Routes,
     useNavigate, 
-    useRoutes
+    useLocation
+    
 } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
@@ -13,57 +14,71 @@ import Book1 from "./Book1.js";
 import Book2 from "./Book2.js";
 import Book3 from "./Book3.js";
 
-function BookSlider() {
+function BookNavigator() {
     const navigate = useNavigate();
-    const [bookIndex, setBookIndex] = useState(1); // Start with Book1
+    const location = useLocation();
+    const books = ["/books/book1", "/books/book2", "/books/book3"];
+    const [currentBookIndex, setCurrentBookIndex] = useState(0);
 
-    const books = [
-        { path: "book1", component: Book1 },
-        { path: "book2", component: Book2 },
-        { path: "book3", component: Book3 }
-    ];
+    useEffect(() => {
+        const currentPath = location.pathname;
+        const bookIndex = books.findIndex(path => path === currentPath);
+        if (bookIndex !== -1) {
+            setCurrentBookIndex(bookIndex);
+        }
+    }, [location, books]);
 
     const navigateToBook = (direction) => {
-        let newBookIndex = bookIndex + direction;
-        if (newBookIndex < 1) newBookIndex = books.length;
-        if (newBookIndex > books.length) newBookIndex = 1;
+        let newBookIndex = currentBookIndex + direction;
+        if (newBookIndex < 0) newBookIndex = books.length - 1;
+        if (newBookIndex >= books.length) newBookIndex = 0;
 
-        setBookIndex(newBookIndex);
-        navigate(`/books/${books[newBookIndex - 1].path}`);
+        navigate(books[newBookIndex]);
     };
-
-    const bookRoutes = books.map((book, index) => ({
-        path: book.path,
-        element: index + 1 === bookIndex ? <book.component /> : null
-    }));
-
-    const routes = useRoutes(bookRoutes);
 
     return (
         <div>
             <button className='img-slider-left-btn' onClick={() => navigateToBook(-1)}></button>
-            {routes}
             <button className='img-slider-right-btn' onClick={() => navigateToBook(1)}></button>
         </div>
     );
 }
 
 function Main() {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+
     return (
         <Router>
             <>
                 <header>
                     <h1><NavLink to="/">M. J. Lindemann</NavLink></h1>
                     <ul className="navbar">
-                        <li><NavLink to="/books/book1">Books</NavLink></li>
+                        <li onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+                            <NavLink to="/books/book1">Books</NavLink>
+                            {showDropdown && (
+                                <ul className="dropdown-menu">
+                                    <li><NavLink to="/books/book1">Book 1</NavLink></li>
+                                    <li><NavLink to="/books/book2">Book 2</NavLink></li>
+                                    <li><NavLink to="/books/book3">Book 3</NavLink></li>
+                                </ul>
+                            )}
+                        </li>
                         <li><NavLink to="/about">About</NavLink></li>
                         <li><a href="https://mjlindemann.blogspot.com/">Blog</a></li>
                     </ul>
                 </header>
                 <main>
+                    <BookNavigator />
                     <Routes>
                         <Route exact path="/" element={<Home />} />
-                        <Route path="/books/*" element={<BookSlider />} />
+                        <Route path="/books/book1" element={<Book1 />} />
+                        <Route path="/books/book2" element={<Book2 />} />
+                        <Route path="/books/book3" element={<Book3 />} />
                         <Route path="/about" element={<About />} />
                     </Routes>
                 </main>
